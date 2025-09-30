439106f222cbbbf0897e8213471e85a4e9961759
Eric Bouchut <ebouchut@gmail.com>
2025-09-30 11:36:32 +0200

    feat(Gson): Add the Gson v3.12.3 external library

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
         Person person = new Person("Eric", 33);
+        // Use compact single-line JSON output
+        //        Gson gson = new Gson();
 
-        System.out.println(person);
+        // Use pretty JSON output with one attribute per line, indentations
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        String personAsJson = gson.toJson(person);
+
+        System.out.println(personAsJson);
     }
 }
